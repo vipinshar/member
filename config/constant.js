f22a5805d273cd580b4e1431091674f1752271f0
vipinshar <vipin_sharma03@hotmail.com>
2017-10-22 16:41:15 +0530

    r

--- config/constant.js
+++ config/constant.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    "M_Url": "mongodb://vipin:vipin%40123@ds129050.mlab.com:29050/mongolabdb",
-    "Key": "Vipin@123!!@%&*%^&",
+    "M_Url": "",
+    "Key": "",
     "Version":"1.0",
     RESPONSE_ENUM: {
         "SUCCESS": 200,
